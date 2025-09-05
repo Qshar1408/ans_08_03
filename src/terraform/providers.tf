608753542bf888c0cb45f654a6208d2e2ada031b
Qshar1408 <slider47@yandex.ru>
2025-09-05 19:25:25 +0300

    Add files via upload

---
+++ src/terraform/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">=0.13"
+}
+
+provider "yandex" {
+
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.default_zone
+  service_account_key_file = file("/home/qshar/ans_08_03/terraform/.authorized_key.json")
+}
